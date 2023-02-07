b0f5b9dbbde64a23b5c8e73272beff7d13b16dc8
damien MATHIEU <damien.trot34@gmail.com>
2023-02-07 19:05:29 +0100

    https://reactjs.org/tutorial/tutorial.html#storing-a-history-of-moves

--- src/index.js
+++ src/index.js
@@ -2,6 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
 function Square(props) {
   return (
     <button className="square" onClick={props.onClick}>
@@ -15,13 +35,19 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = "X";
-    this.setState({ squares: squares });
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext
+      ? "X"
+      : "O"; /*Conditional (ternary) operator*/
+    this.setState({ squares: squares, xIsNext: !this.state.xIsNext });
   }
 
   renderSquare(i) {
@@ -34,7 +60,15 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = "Next player: X";
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = "Winner: " + winner;
+    } else if (!this.state.squares.includes(null)) {
+      status = "Draw";
+    } else {
+      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+    }
 
     return (
       <div>
